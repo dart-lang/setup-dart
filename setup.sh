8307b7f4436d6be1ef56413d94ce202b93d23a4d
Istvan Soos <istvan.soos@gmail.com>
2022-11-29 16:01:36 +0100

    export PUB_TOKEN before calling pub token add

--- setup.sh
+++ setup.sh
@@ -112,6 +112,7 @@ then
   then
     PUB_TOKEN=$(curl --retry 5 --retry-connrefused -sLS "${ACTIONS_ID_TOKEN_REQUEST_URL}&audience=https://pub.dev" -H "User-Agent: actions/oidc-client" -H "Authorization: Bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" | jq -r '.value')
     echo "PUB_TOKEN=${PUB_TOKEN}" >> $GITHUB_ENV
+    export PUB_TOKEN
     ${RUNNER_TOOL_CACHE}/dart-sdk/bin/dart pub token add https://pub.dev --env-var PUB_TOKEN
   else
     echo "Could not setup OIDC token, 'jq' is not available.";
